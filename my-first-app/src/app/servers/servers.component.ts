b53100acc61473cd9dad779817e10bdc1f5e4820
Minhaj9800 <ridwanminhaj@gmail.com>
2022-11-05 19:35:08 -0300

    Discuss about select by attribute, class for slector

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+
+  selector: 'app-servers', // Select By element
+  //selector: '[app-servers]',// To select by attribute
+  //selector: '.app-servers', // Select By class
+  //Note: Select by Id won't work in Angular
   //using inline template
   template: `<app-server></app-server>
   <app-server></app-server>`,
