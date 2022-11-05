93748e9ff88f042bfe00176c8df71a7c499d485a
Minhaj9800 <ridwanminhaj@gmail.com>
2022-11-05 18:18:31 -0300

    Inline style and Inline templating discussed

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -3,7 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css']
+  //inline styles
+  styles: [
+    `
+      h3 {
+        color: dodgerblue;
+      }
+    `,
+
+    `
+      app-servers {
+        color: red;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   //name = 'Minhaj';
