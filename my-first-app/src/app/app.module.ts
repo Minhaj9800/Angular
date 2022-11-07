c3063c4b819ae5983dc1933cc3d594547379db9e
Minhaj9800 <ridwanminhaj@gmail.com>
2022-11-06 22:27:18 -0400

    Discuss two way binding

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-// import {FormsModule} from '@angular/forms';
+import {FormsModule} from '@angular/forms';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { warningAlertComponent } from './warning-alert/warning-alert.component';
@@ -18,7 +18,7 @@ import { SuccessAlertComponent } from './success-alert/success-alert.component';
   ],
   imports: [
     BrowserModule,
-    // FormsModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
