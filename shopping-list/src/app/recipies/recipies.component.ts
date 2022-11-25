63327c2db416aaec2cfdb6fb90c0be5904fe1006
Minhaj9800 <ridwanminhaj@gmail.com>
2022-11-25 01:52:51 -0400

    Create recipe model

--- shopping-list/src/app/recipies/recipies.component.ts
+++ shopping-list/src/app/recipies/recipies.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-recipies',
   templateUrl: './recipies.component.html',
-  styleUrls: ['./recipies.component.css']
+  styleUrls: ['./recipies.component.css'],
 })
 export class RecipiesComponent implements OnInit {
+  recipies = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
